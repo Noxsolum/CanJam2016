65e3fc0962dfb5aea862ab0da97c33390e1dcdf8
Steven Hooke <stevenhooke@hotmail.co.uk>
2016-03-19 19:50:03 +0000

    Random Panel and Gap Generation (up to 10 panels)

---
+++ CanJam2016/Assets/Scripts/PanelManager.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PanelManager : MonoBehaviour
+{
+    public GameObject tilePrefab;
+    public GameObject currentPanel;
+
+	// Use this for initialization
+	void Start ()
+    {
+        for (int i = 0; i < 10; i++)
+        {
+            spawnPanel();
+        }
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+    public void spawnPanel()
+    {
+        currentPanel = (GameObject)Instantiate(tilePrefab, currentPanel.transform.GetChild(0).transform.GetChild(Random.Range(0, 2)).position, Quaternion.identity);
+
+    }
+}

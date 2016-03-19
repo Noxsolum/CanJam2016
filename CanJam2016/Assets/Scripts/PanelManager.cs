999c954690c90e20963b1c1cf6669f0591286b34
Zach Jones <z_jones@hotmail.co.uk>
2016-03-19 21:14:57 +0000

    The semblance of an actual game

---
+++ CanJam2016/Assets/Scripts/PanelManager.cs
@@ -0,0 +1,36 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PanelManager : MonoBehaviour
+{
+    public GameObject tilePrefab;
+    public GameObject currentPanel;
+    public GameObject[] prefabs;
+
+	// Use this for initialization
+	void Start ()
+    {
+        for (int i = 0; i < 10; i++)
+        {
+            spawnPanel();
+        }
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+    public void spawnPanel()
+    {
+        int rand = Random.Range(0, 2);
+        currentPanel = (GameObject)Instantiate(tilePrefab, currentPanel.transform.GetChild(0).transform.GetChild(rand).position, Quaternion.identity);
+        if (rand == 0)
+        {
+            int prefabNum = 0; //Random.Range(0, 3);
+            Instantiate(prefabs[prefabNum], currentPanel.transform.GetChild(0).transform.GetChild(rand).position, Quaternion.identity);
+        }
+        else { }
+
+    }
+}

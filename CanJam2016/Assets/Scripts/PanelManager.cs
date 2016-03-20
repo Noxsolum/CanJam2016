b2f85514db44c40a407959ae3c3b608048db1d46
Steven Hooke <stevenhooke@hotmail.co.uk>
2016-03-20 11:42:58 +0000

    InfiniType v1.2
    - Final build (sans .exe)

--- CanJam2016/Assets/Scripts/PanelManager.cs
+++ CanJam2016/Assets/Scripts/PanelManager.cs
@@ -10,11 +10,14 @@ public class PanelManager : MonoBehaviour
     public DestroyObject scene;
 
     private int fenceSpawnDistance;
+    private bool leftCornerSpawned, rightCornerSpawned;
 
 	// Use this for initialization
 	void Start ()
     {
         fenceSpawnDistance = 0;
+        leftCornerSpawned = true;
+        rightCornerSpawned = true;
 
         for (int i = 0; i < 10; i++)
         {
@@ -31,12 +34,36 @@ public class PanelManager : MonoBehaviour
 
     public void spawnPanel()
     {
-        int rand = Random.Range(0, 2);
+        int rand = Random.Range(0, 3);
         int rand2 = Random.Range(0, 10);
-        Vector3 position = new Vector3((currentPanel.transform.GetChild(0).transform.GetChild(rand).position.x) + 0.0f, (currentPanel.transform.GetChild(0).transform.GetChild(rand).position.y), (currentPanel.transform.GetChild(0).transform.GetChild(rand).position.z));;
-        Vector3 fencePosition = new Vector3((currentPanel.transform.GetChild(0).transform.GetChild(rand).position.x), (currentPanel.transform.GetChild(0).transform.GetChild(rand).position.y) + 1.5f, (currentPanel.transform.GetChild(0).transform.GetChild(rand).position.z) - 7.0f);
+        int rand3 = Random.Range(0, 2);
+        int index = 0;
+        Vector3 position = new Vector3(currentPanel.transform.GetChild(0).transform.GetChild(0).position.x, currentPanel.transform.GetChild(0).transform.GetChild(0).position.y, (currentPanel.transform.GetChild(0).transform.GetChild(0).position.z));
+        Vector3 fencePosition = new Vector3((currentPanel.transform.GetChild(0).transform.GetChild(0).position.x), (currentPanel.transform.GetChild(0).transform.GetChild(0).position.y) + 1.5f, (currentPanel.transform.GetChild(0).transform.GetChild(0).position.z) - 7.0f);
         
-        if (rand2 == 0 && fenceSpawnDistance < 0)
+        if (rand2 == 2 && !leftCornerSpawned && !rightCornerSpawned)
+        {
+            index = 1;
+            leftCornerSpawned = true;
+            rightCornerSpawned = true;
+        }
+        else
+        {
+            
+        }
+
+        if (rand2 == 4 && !rightCornerSpawned && !leftCornerSpawned)
+        {
+            index = 2;
+            rightCornerSpawned = true;
+            leftCornerSpawned = true;
+        }
+        else
+        {
+            
+        }
+
+        if (rand2 == 0 && fenceSpawnDistance < 0 && !rightCornerSpawned && !leftCornerSpawned)
         {
             //Fence
             int prefabNum = 0; //Random.Range(0, 3);
@@ -44,15 +71,21 @@ public class PanelManager : MonoBehaviour
 
             scene.numOfObjects++;
             fenceSpawnDistance = 5;
+
+            leftCornerSpawned = false;
+            rightCornerSpawned = false;
         }
         else if(rand2 > 5)
         {
             //Pit
-            position = new Vector3((currentPanel.transform.GetChild(0).transform.GetChild(rand).position.x) + 0.0f, (currentPanel.transform.GetChild(0).transform.GetChild(rand).position.y), (currentPanel.transform.GetChild(0).transform.GetChild(rand).position.z) + 3.0f);
+            position = new Vector3((currentPanel.transform.GetChild(0).transform.GetChild(0).position.x), (currentPanel.transform.GetChild(0).transform.GetChild(0).position.y), (currentPanel.transform.GetChild(0).transform.GetChild(0).position.z) + 1.5f);
+
+            leftCornerSpawned = false;
+            rightCornerSpawned = false;
         }
 
         fenceSpawnDistance--;
         
-        currentPanel = (GameObject)Instantiate(tilePrefab[rand], position, Quaternion.identity);
+        currentPanel = (GameObject)Instantiate(tilePrefab[index], position, Quaternion.identity);
     }
 }

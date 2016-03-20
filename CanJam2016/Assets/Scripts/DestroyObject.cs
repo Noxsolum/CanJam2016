620e13a069e83706216ac3641883bf8d42d300ec
Zach Jones <z_jones@hotmail.co.uk>
2016-03-20 07:23:41 +0000

    Fixed Paths, UI and General Design

--- CanJam2016/Assets/Scripts/DestroyObject.cs
+++ CanJam2016/Assets/Scripts/DestroyObject.cs
@@ -1,25 +1,32 @@
 ﻿using UnityEngine;
 using System.Collections;
+using UnityEngine.UI;
 
 public class DestroyObject : MonoBehaviour
 {
     public int numOfObjects;
+    [SerializeField] public Text scoreText;
+    [SerializeField] public Text gameOver;
+    int score;
 
 	// Use this for initialization
 	void Start ()
     {
         numOfObjects = 0;
+        score = 0;
 	}
 	
 	// Update is called once per frame
 	void Update ()
     {
-	
+        if(gameOver.text != "Game Over")
+            scoreText.text = "Score: " + score;
 	}
 
     void OnTriggerEnter(Collider col)
     {
         Destroy(col.gameObject);
         numOfObjects--;
+        score++;
     }
 }
--- CanJam2016/Assets/Scripts/PanelManager.cs
+++ CanJam2016/Assets/Scripts/PanelManager.cs
@@ -30,17 +30,22 @@ public class PanelManager : MonoBehaviour
     public void spawnPanel()
     {
         int rand = Random.Range(0, 2);
-        Vector3 position = new Vector3((currentPanel.transform.GetChild(0).transform.GetChild(rand).position.x) + 0.0f, (currentPanel.transform.GetChild(0).transform.GetChild(rand).position.y), (currentPanel.transform.GetChild(0).transform.GetChild(rand).position.z) + 2.0f);
+        int rand2 = Random.Range(0, 10);
+        Vector3 position = new Vector3((currentPanel.transform.GetChild(0).transform.GetChild(rand).position.x) + 0.0f, (currentPanel.transform.GetChild(0).transform.GetChild(rand).position.y), (currentPanel.transform.GetChild(0).transform.GetChild(rand).position.z));;
         Vector3 fencePosition = new Vector3((currentPanel.transform.GetChild(0).transform.GetChild(rand).position.x), (currentPanel.transform.GetChild(0).transform.GetChild(rand).position.y) + 1.5f, (currentPanel.transform.GetChild(0).transform.GetChild(rand).position.z) - 7.0f);
-        currentPanel = (GameObject)Instantiate(tilePrefab[rand], position, Quaternion.identity);
-        if (rand == 0)
+        
+        if (rand2 == 0)
         {
             int prefabNum = 0; //Random.Range(0, 3);
             Instantiate(prefabs[prefabNum], fencePosition, Quaternion.identity);
 
             scene.numOfObjects++;
         }
-        else { }
+        else if(rand2 > 5)
+        {
+            position = new Vector3((currentPanel.transform.GetChild(0).transform.GetChild(rand).position.x) + 0.0f, (currentPanel.transform.GetChild(0).transform.GetChild(rand).position.y), (currentPanel.transform.GetChild(0).transform.GetChild(rand).position.z) + 2.5f);
+        }
 
+        currentPanel = (GameObject)Instantiate(tilePrefab[rand], position, Quaternion.identity);
     }
 }

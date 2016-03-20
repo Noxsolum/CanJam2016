6554d7d4f76957076d829684feb748094171ed01
Zach Jones <z_jones@hotmail.co.uk>
2016-03-20 01:03:37 +0000

    Infinite Map Gen

--- CanJam2016/Assets/Scripts/DestroyObject.cs
+++ CanJam2016/Assets/Scripts/DestroyObject.cs
@@ -1,20 +1,25 @@
 ﻿using UnityEngine;
 using System.Collections;
 
-public class DestroyObject : MonoBehaviour {
+public class DestroyObject : MonoBehaviour
+{
+    public int numOfObjects;
 
 	// Use this for initialization
-	void Start () {
-	
+	void Start ()
+    {
+        numOfObjects = 0;
 	}
 	
 	// Update is called once per frame
-	void Update () {
+	void Update ()
+    {
 	
 	}
 
     void OnTriggerEnter(Collider col)
     {
         Destroy(col.gameObject);
+        numOfObjects--;
     }
 }
--- CanJam2016/Assets/Scripts/PanelManager.cs
+++ CanJam2016/Assets/Scripts/PanelManager.cs
@@ -6,10 +6,13 @@ public class PanelManager : MonoBehaviour
     public GameObject tilePrefab;
     public GameObject currentPanel;
     public GameObject[] prefabs;
+    public DestroyObject scene;
 
 	// Use this for initialization
 	void Start ()
     {
+        //scene = new global::DestroyObject();
+
         for (int i = 0; i < 10; i++)
         {
             spawnPanel();
@@ -17,9 +20,11 @@ public class PanelManager : MonoBehaviour
 	}
 	
 	// Update is called once per frame
-	void Update () {
-	
-	}
+	void Update ()
+    {
+        if (scene.numOfObjects < 10)
+            spawnPanel();
+    }
 
     public void spawnPanel()
     {
@@ -28,7 +33,11 @@ public class PanelManager : MonoBehaviour
         if (rand == 0)
         {
             int prefabNum = 0; //Random.Range(0, 3);
-            Instantiate(prefabs[prefabNum], currentPanel.transform.GetChild(0).transform.GetChild(rand).position, Quaternion.identity);
+            Vector3 position = new Vector3 (currentPanel.transform.GetChild(0).transform.GetChild(rand).position.x, 1.5f, currentPanel.transform.GetChild(0).transform.GetChild(rand).position.z);
+
+            Instantiate(prefabs[prefabNum], position, Quaternion.identity);
+
+            scene.numOfObjects++;
         }
         else { }
 
